ffddb99948f15bac277a696c3c0748754bb53824
tandinho <32461398+hodinhtan@users.noreply.github.com>
2023-03-22 12:40:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,10 @@ if not os.path.isdir("/tmp/ta-lib"):
 from ctypes import *
 
 #lib = CDLL(os.path.abspath("libta_lib.so.0.0.0"))
-run_command(["ls", "-l","/home/appuser/venv/lib/"])
+run_command(["ls", "-l","/home/appuser/venv/lib/python3.9"])
+# run_command(["ls", "-l","/tmp"])
+run_command(["ls", "-l","/user/local/lib"])
+run_command(["ls", "-l","/tmp"])
 lib = CDLL("/home/appuser/venv/lib/libta_lib.so.0.0.0")
 # import library
 try:
