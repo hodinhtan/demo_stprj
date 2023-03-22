f5d4e610b7bd5f54faeba21f76c08a846f0ceca5
tandinho <32461398+hodinhtan@users.noreply.github.com>
2023-03-22 14:55:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -81,7 +81,8 @@ run_command(["ls", "-a","/home/appuser/include/ta-lib/ta_defs.h"])
 try:
     import talib
 except ImportError:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "--global-option=build_ext", "--global-option=-L/home/appuser/lib/", "--global-option=-I/home/appuser/include/", "ta-lib"])
+    subprocess.run(["pip", "install", "ta-lib")
+   # subprocess.check_call([sys.executable, "-m", "pip", "install", "--global-option=build_ext", "--global-option=-L/home/appuser/lib/", "--global-option=-I/home/appuser/include/", "ta-lib"])
 finally:
     import talib
 
